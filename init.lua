228331eb5bdae4d2243ccdaf79db1a78bbf3f54c
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-18 17:47:15 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
+origins = {}
+
 local mp = minetest.get_modpath(minetest.get_current_modname())
+
+origins.origins = {
+  enderian = {},
+  merling = {},
+  phantom = {},
+  elytrian = {},
+  blazeborn = {},
+  avain = {},
+  arachnid = {},
+  shulk = {},
+  feline = {},
+}
