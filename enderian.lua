8f1ba4129c5055fdf1b961a18338c8249a5f179d
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-20 21:51:09 +0100

    Update enderian.lua

--- enderian.lua
+++ enderian.lua
@@ -1,9 +1,9 @@
 minetest.register_globalstep(function(dtime)
-  for _, pname in pairs(origins.origin.enderian) do
+	for _, pname in pairs(origins.origin.enderian) do
 		local player = minetest.get_player_by_name(pname)
 		local control = player:get_player_control()
-    if control.zoom then
-     mcl_throwing.get_player_throw_function("mcl_throwing:ender_pearl_entity")
-    end
-  end
+		if control.zoom then
+			mcl_throwing.get_player_throw_function("mcl_throwing:ender_pearl_entity")
+		end
+	end
 end)
