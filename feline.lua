5cbd8f9bfc3796e6d825e4f53066dbf717ce9328
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-20 16:47:50 +0100

    Update feline.lua

--- feline.lua
+++ feline.lua
@@ -2,11 +2,12 @@ minetest.register_on_player_hpchange(function(player, hp_change, reason)
 	if not player then return end
 	local name = player:get_player_name()
 	if hp_change < 0 then
-		local fall_dmg
+		local fall_dmg = false
 		local team = origins.get_player_team(name)
 		if reason.type == "fall" and team == "feline" then
-			fall_dmg = 0
+			hp_change = 0
 		end
 	end
-	return fall_dmg or hp_change
+	hp_change = -math.abs(hp_change)
+	return hp_change
 end, true)
