933c7e91583fd0067c56bbd52cdcff0498214f05
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-20 17:05:15 +0100

    Update blazeborn.lua

--- blazeborn.lua
+++ blazeborn.lua
@@ -1,10 +1,10 @@
 minetest.register_on_player_hpchange(function(player, hp_change, reason)
-  if not player then return end
-  local name = player:get_player_name()
+	if not player then return end
+	local name = player:get_player_name()
 	if hp_change < 0 then
 		local team = origins.get_player_team(name)
 		if (damage_type == "burning" or damage_type == "fireball" or reason.type == "node_damage" and 
-          (reason.node == "mcl_fire:fire" or reason.node == "mcl_core:lava_source" or reason.node == "mcl_core:lava_flowing")) and team == "blazeborn" then
+		(reason.node == "mcl_fire:fire" or reason.node == "mcl_core:lava_source" or reason.node == "mcl_core:lava_flowing")) and team == "blazeborn" then
 			hp_change = 0
 		end
 	end
