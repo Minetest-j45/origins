18ad11aed8704374b29ee04e5133f927350dd95c
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-20 16:11:17 +0100

    no fall dmg for feline

--- feline.lua
+++ feline.lua
@@ -1,1 +1,10 @@
-
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+    if not player then return end
+    local name = player:get_player_name()
+    if hp_change < 0 then
+        local team = origins.get_player_team(name)
+        if reason.type == "fall" and team == "feline" then
+					return 0
+				end
+    end
+end)
