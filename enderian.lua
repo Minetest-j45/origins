f81811171dd324c26ee10ddfeda54a983cef530e
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-21 16:53:52 +0100

    Delete enderian.lua

--- enderian.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_globalstep(function(dtime)
-	for _, pname in pairs(origins.origin.enderian) do
-		local player = minetest.get_player_by_name(pname)
-		local control = player:get_player_control()
-		if control.jump then
-			mcl_throwing.get_player_throw_function("mcl_throwing:ender_pearl_entity")
-		end
-	end
-end)
