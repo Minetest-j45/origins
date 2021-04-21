06603caa1bdb992ec5bde3d9a26e0579bc1e7667
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-21 16:48:34 +0100

    blazeborn take fireball dmg

--- blazeborn.lua
+++ blazeborn.lua
@@ -3,7 +3,7 @@ minetest.register_on_player_hpchange(function(player, hp_change, reason)
 	local name = player:get_player_name()
 	if hp_change < 0 then
 		local team = origins.get_player_team(name)
-		if (damage_type == "burning" or damage_type == "fireball" or reason.type == "node_damage" and 
+		if (damage_type == "burning" or reason.type == "node_damage" and 
 		(reason.node == "mcl_fire:fire" or reason.node == "mcl_core:lava_source" or reason.node == "mcl_core:lava_flowing")) and team == "blazeborn" then
 			hp_change = 0
 		end
