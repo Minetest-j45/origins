5d697cdee467a31d57b311720cf09e3f4509d9db
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-21 17:43:11 +0100

    Update enderian.lua

--- enderian.lua
+++ enderian.lua
@@ -1,1 +1,17 @@
+local GRAVITY = tonumber(minetest.settings:get("movement_gravity"))
+
+minetest.register_globalstep(function(dtime)
+	for _, pname in ipairs(origins.origin.enderian) do
+		local player = minetest.get_player_by_name(pname)
+		local control = player:get_player_control()
+		if control.jump then
+			local pos = player:get_pos()
+      local dir = player:get_look_dir()
+      local obj = minetest.add_entity(pos, "mcl_throwing:ender_pearl_entity")
+      obj:set_velocity(vector.multiply(dir, 22))
+      obj:set_acceleration({x=dir.x*-3, y=-GRAVITY, z=dir.z*-3})
+		end
+	end
+end)
+
 
