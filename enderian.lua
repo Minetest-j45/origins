25bfbc793d3aad87621b55d4611660617a27d4c7
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-27 18:11:54 +0100

    add debug stuff

--- enderian.lua
+++ enderian.lua
@@ -170,6 +170,7 @@ minetest.register_globalstep(function(dtime)
 			local headpos = vector.add(player:get_pos(), {x = 0, y = 1.625, z = 0})
 			local legpos = player:get_pos()
 			if minetest.get_item_group(minetest.get_node(headpos), "water") ~= 0 or minetest.get_item_group(minetest.get_node(legpos), "water") ~= 0 then
+				minetest.chat_send_all("yaya")
 				player:set_hp(player:get_hp()-math.random(0.5, 1), { type = "allergies", from = "mod" })
 			end
 		end
