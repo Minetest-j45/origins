b8ee780461046f76d30f5d97e6bdc80a994bfd97
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-04-27 21:17:08 +0100

    Update enderian.lua

--- enderian.lua
+++ enderian.lua
@@ -163,14 +163,11 @@ local water_timer = 0
 minetest.register_globalstep(function(dtime)
 	water_timer = water_timer + dtime;
 	if water_timer >= 1 then
-		for _,player in pairs(minetest.get_connected_players()) do
+		for _, player in pairs(minetest.get_connected_players()) do
 			local pname = player:get_player_name()
 			local team = origins.get_player_team(pname)
 			if team ~= "enderian" then return end
-			local headpos = vector.add(player:get_pos(), {x = 0, y = 1, z = 0})
-			local legpos = player:get_pos()
-			minetest.chat_send_all(dump(minetest.get_item_group(minetest.get_node(headpos), "water")))
-			if minetest.get_item_group(minetest.get_node(headpos), "water") > 0 or minetest.get_item_group(minetest.get_node(legpos), "water") > 0 then
+			if minetest.get_item_group(mcl_playerinfo[name].node_head, "water") ~= 0 or minetest.get_item_group(mcl_playerinfo[name].node_feet, "water") > 0 then
 				minetest.chat_send_all("yaya")
 				player:set_hp(player:get_hp()-math.random(0.5, 1), { type = "allergies", from = "mod" })
 			end
